0a82ca73ce5bc56cbf9adcf32c43faf8f3261e95
AcilRestu12 <arieskarestu214@gmail.com>
2024-07-24 21:40:37 +0700

    front-end -> nav-app & home page | micro change

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	return render_template("pages/index.html")
 
 @app.route("/chat")
 def chat():
-	return render_template("chat.html")
+	return render_template("pages/chat.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
